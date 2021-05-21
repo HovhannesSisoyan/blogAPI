d14bc89aacf093f2f63ef16e3ebe33cf64614c68
hovhannessisoyan <hovosisoyan@gmail.com>
2021-05-21 15:17:16 +0400

    project setup

---
+++ blog/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using  Microsoft.AspNetCore.Cors ;
+using Microsoft.Extensions.Logging;
+
+namespace blog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [EnableCors("CorsPolicy")]
+        [HttpGet("/")]
+
+        [HttpGet]
+        public string Get()
+        {
+            var rng = new Random();
+            return "Home page data";
+        }
+    }
+}
